fd9b4b50acc75aeb5737c38322d83b0f9b36ecc6
Connor Makowski <connorman528@gmail.com>
2022-04-21 14:51:04 -0400

    Simplify test_0.py

--- test/test_0.py
+++ test/test_0.py
@@ -1,13 +1,9 @@
 from scoptimize.network import Model, Node, Flow
-from pprint import pp
 
 model = Model(name='MyModel')
-
 model.add_object(Node(name="Factory_1", origin=True, cashflow_per_unit=-1, max_units=15))
 model.add_object(Node(name="Customer_1", destination=True, min_units=10))
-
 model.add_object(Flow(name="Factory_1__Customer_1", cashflow_per_unit=-1, max_units=15, start='Factory_1', end='Customer_1'))
-
 model.solve()
 
 print(model.objective)
